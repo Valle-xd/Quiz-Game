4f920f320e5b248121ff490213d3674b34d18638
Valle-xd <martin_rydahl@hotmail.com>
2020-06-22 23:42:06 +0200

    questions file

---
+++ questions.js
@@ -0,0 +1,151 @@
+const questions = [
+    {
+        question: 'Hvad hedder den største ø i Danske Kongerige?',
+        
+        answers: [
+        {text: 'Sjælland', correct: false},
+        {text: 'Bornholm', correct: false},
+        {text: 'Grønland', correct: true},
+        {text: 'Lolland', correct: false}
+        ]
+    },
+    {
+        question: 'Hvad hedder det største hav i hele verden?',
+        
+        answers: [
+        {text: 'Stillehavet', correct: true},
+        {text: 'Middelhavet', correct: false},
+        {text: 'Atlanterhavet', correct: false},
+        {text: 'Det indiske Ocean', correct: false}
+        ]
+    },
+    {
+        question: 'Hvor mange knogler er der i en menneskekrop?',
+        
+        answers: [
+        {text: '620', correct: false},
+        {text: '135', correct: false},
+        {text: '440', correct: false},
+        {text: '206', correct: true}
+        ]
+    },
+    {
+        question: 'Fødes kaniner blinde?',
+        
+        answers: [
+        {text: 'Ja', correct: true},
+        {text: 'Nej', correct: false}
+        ]
+    },
+    {
+        question: 'Hvor mange stjerner er der i det amerikanske flag?',
+        
+        answers: [
+        {text: '51', correct: true},
+        {text: '44', correct: false},
+        {text: '29', correct: false},
+        {text: '38', correct: false}
+        ]
+    },
+    {
+        question: 'Hvad er det største kattedyr i hele verden?',
+        
+        answers: [
+        {text: 'Puma', correct: false},
+        {text: 'Leopard', correct: false},
+        {text: 'Tiger', correct: true},
+        {text: 'Jaguar', correct: false}
+        ]
+    }, 
+    {
+        question: 'Nogle måneder har 31 dage, mens andre har 30. Hvor mange af dem har 28 dage?',
+        
+        answers: [
+        {text: '1', correct: false},
+        {text: '10', correct: false},
+        {text: '4', correct: false},
+        {text: '12', correct: true}
+        ]
+    },
+    {
+        question: 'Hvor gammel skal man være, før man kan tage kørekort til hhv. stor bil og motorcykel?',
+        
+        answers: [
+        {text: '30 år', correct: false},
+        {text: '24 år', correct: false},
+        {text: '21 år', correct: true},
+        {text: '18 år', correct: false}
+        ]
+    },
+    {
+        question: 'Hvor langt skal man gå for at forbrænde en Mars-bar?',
+        
+        answers: [
+        {text: '3 til 4 km', correct: false},
+        {text: '7 til 8 km', correct: false},
+        {text: '2 til 3 km', correct: false},
+        {text: '5 til 6 km', correct: true}
+        ]
+    },
+    {
+        question: 'Hvor høj er Frihedsgudinden i New York?',
+        
+        answers: [
+        {text: '42 meter', correct: false},
+        {text: '54 meter', correct: false},
+        {text: '25 meter', correct: false},
+        {text: '55 meter', correct: true}
+        ]
+    },
+    {
+        question: 'I 2019 tog vi afsked med en af Danmarks største erhvervsmænd, Lars Larsen. I hvilken by åbnede han i 1979 den allerførste Jysk Sengetøjslager på Silkeborgvej?',
+        
+        answers: [
+        {text: 'Esbjerg', correct: false},
+        {text: 'Aarhus', correct: true},
+        {text: 'Aalborg', correct: false},
+        {text: 'Vejle', correct: false}
+        ]
+    },
+    {
+        question: 'I hvilket år fik Aarhus sit navn med dobbelt A?',
+        
+        answers: [
+        {text: '1980', correct: false},
+        {text: '2006', correct: false},
+        {text: '1994', correct: false},
+        {text: '2011', correct: true}
+        ]
+    },  
+    {
+        question: 'Fra hvilken land stammer Amarone vine fra?',
+        
+        answers: [
+        {text: 'Italien', correct: true},
+        {text: 'Spanien', correct: false},
+        {text: 'Grækenland', correct: false},
+        {text: 'Portugal', correct: false}
+        ]
+    },
+    {
+        question: 'Hvor mange øer består Færøerne af? ',
+        
+        answers: [
+        {text: '15', correct: false},
+        {text: '10', correct: false},
+        {text: '25', correct: false},
+        {text: '18', correct: true}
+        ]
+    },
+    {
+        question: 'Hvilket dyr slår flest mennesker ihjel?',
+        
+        answers: [
+        {text: 'Løver', correct: false},
+        {text: 'Malariamyggen', correct: true},
+        {text: 'Hajer', correct: false},
+        {text: 'Bjørne', correct: false}
+        ]
+    },
+
+]
